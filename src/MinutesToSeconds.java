56912eb982472ef698dd570d5f86367ed612009d
Wendy Robins <wendy@therobins.net>
2021-12-16 13:06:29 -0500

    added more algorithms

---
+++ src/MinutesToSeconds.java
@@ -0,0 +1,13 @@
+public class MinutesToSeconds {
+    public static void main(String[] args) {
+
+    }
+    //Write a function that takes an integer minutes and converts it to seconds.
+    //public is the access modifier
+    //static
+    //int type
+    public static int minutesToSeconds (int minutes) {
+        return minutes * 60;
+    }
+
+}
---
+++ src/ReturnSomethingToMe.java
@@ -0,0 +1,12 @@
+public class ReturnSomethingToMe {
+    public static void main(String[] args) {
+
+    }
+    /*
+    Write a function that returns the string "something" joined
+    with a space " " and the given argument a.
+    */
+    public static String giveMeSomething(String a) {
+        return "something " + a;
+    }
+}
