f41b3c5a3228e5986dfccb144a974aae29074b1c
Wendy Robins <wendy@therobins.net>
2021-12-21 12:28:53 -0500

    updated algorithms

--- src/GetWordCount.java
+++ src/GetWordCount.java
@@ -8,15 +8,17 @@ public class GetWordCount {
     //count. The string will be a sentence.
 
     public static int countWords(String s) {
-        int count = 1;
-        for (int i = 0; i < s.length() - 1; i++){
-            if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
-            {
-                count++;
-            }
+//        int count = 1;
+//        for (int i = 0; i < s.length() - 1; i++){
+//            if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
+//            {
+//                count++;
+//            }
+//
+//        }
+//        return count;
 
-        }
-        return count;
+        return s.split(" ").length;
     }
 
 }
