4b344d87182384333e0e4b97cdf3e8eb8cc31c4c
Wendy Robins <wendy@therobins.net>
2021-12-23 12:54:27 -0500

    function that takes array and returns difference between biggest and smallest numbers

--- src/DifferenceMaxMin.java
+++ src/DifferenceMaxMin.java
@@ -1,4 +1,19 @@
+import java.util.Arrays;
+
 public class DifferenceMaxMin {
 
+    public static void main(String[] args) {
+        System.out.println("test " + differenceMaxMin(new int[] {1,2,3,4,50}));
+    }
+
+    //Create a function that takes an array and returns
+    // the difference between the biggest and smallest numbers.
+    public static int differenceMaxMin(int[] arr) {
+
+        //
+        Arrays.sort(arr);  //assigns array to sorted array
+        return arr[arr.length-1] - arr[0];
+
+    }
 
 }
