c552900f9f627d38ed408ab9d015002ddbf6e805
Wendy Robins <wendy@therobins.net>
2021-12-23 12:58:36 -0500

    additional way to iterate to find highest and lowest values and calculate the difference between max and min

--- src/DifferenceMaxMin.java
+++ src/DifferenceMaxMin.java
@@ -11,8 +11,21 @@ public class DifferenceMaxMin {
     public static int differenceMaxMin(int[] arr) {
 
         //
-        Arrays.sort(arr);  //assigns array to sorted array
-        return arr[arr.length-1] - arr[0];
+        //Arrays.sort(arr);  //assigns array to sorted array
+        //return arr[arr.length-1] - arr[0];
+
+        //for loop version
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+            if (arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return max - min;
 
     }
 
