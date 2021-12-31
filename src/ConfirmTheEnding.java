a0543ff1250d89707c1fa2678f4f6f6678e1869e
Wendy Robins <wendy@therobins.net>
2021-12-30 20:37:10 -0500

    added another algorithm that checks if a string ends with a given target string

--- src/ConfirmTheEnding.java
+++ src/ConfirmTheEnding.java
@@ -1,10 +1,29 @@
+import java.util.Arrays;
+
 public class ConfirmTheEnding {
 
+    public static void main(String[] str) {
+        System.out.println("Does string end with given target? " + confirmEnding("Boston", "on"));
+    }
 
     /*
+    converted this from javascript to java
     Check if a string (first argument, str) ends with the given target string (second argument, target).
 
-This challenge can be solved with the .endsWith() method, which was introduced in ES2015.
-But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+    This challenge can be solved with the .endsWith() method, which was introduced in ES2015.
+    But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
      */
+
+    public static String confirmEnding(String str, String target) {
+
+        int strLength = str.length();
+        int targetLength = target.length();
+        if (str.substring(strLength - targetLength).equals(target)) {
+            str = "true";
+        } else{
+            str = "false";
+        }
+        return str;
+    }
+
 }
