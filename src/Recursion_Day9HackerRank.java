052303b96d4b4d878fef0dfde669d2c9c6017b27
Wendy Robins <wendy@therobins.net>
2022-01-17 14:59:28 -0500

    added recursion

---
+++ src/Recursion_Day9HackerRank.java
@@ -0,0 +1,12 @@
+public class Recursion_Day9HackerRank {
+
+    //Complete the factorial function in the editor below. Be sure to use recursion.
+    public static void main(String[] args) {
+        System.out.println(factorial(3));
+    }
+    public static int factorial(int n) {
+
+        return (n * (n-1));
+    }
+
+}
