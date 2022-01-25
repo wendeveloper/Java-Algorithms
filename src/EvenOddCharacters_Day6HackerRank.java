50a8ab05eb04067bcfc397b9a2106ea7290a54c0
Wendy Robins <wendy@therobins.net>
2022-01-25 11:44:11 -0500

    Day 6 HackerRank java algorithm

---
+++ src/EvenOddCharacters_Day6HackerRank.java
@@ -0,0 +1,44 @@
+//import java.io.*;
+//import java.util.*;
+//
+    public class EvenOddCharacters_Day6HackerRank {
+
+        public static void main(String[] args) {
+
+        }
+//
+//
+//            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+//            Scanner scanner = new Scanner(System.in);
+//            int T = scanner.nextInt(); // number of test cases
+//
+//            for( int i = 0; i < T; i++){
+//                String S = scanner.next();  // read the input and assign to String S
+//                String evenS = "";
+//                String oddS = "";
+//
+//                // convert string S into an array of characters CHAR
+//                char[] arrS = S.toCharArray();
+//
+//                // loop through arrS of characters
+//                for (int j = 0; j < arrS.length; j++) {
+//                    // get the even index that has 0 remainder after divided by 2
+//                    if (j % 2 == 0) {
+//                        //System.out.print(arrS[j]);  // the element at even index
+//                        evenS += arrS[j];
+//                    }
+//                    //odd index
+//                    if (j % 2 != 0) {
+//                        //System.out.print(arrS[j]);  // the element at odd index
+//                        oddS += arrS[j];
+//                    }
+//                }
+//                System.out.println(evenS + " " + oddS);
+//            }
+//
+//
+//        }
+//
+//    }
+//
+}
