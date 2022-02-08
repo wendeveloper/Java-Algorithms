b1b2c0a22a9064d9183b873a9295396cd6136c27
Wendy Robins <wendy@therobins.net>
2022-02-08 16:33:37 -0500

    countdown and countdown recursion

---
+++ src/Countdown.java
@@ -0,0 +1,14 @@
+public class Countdown {
+
+        public static void main(String[] args) {
+
+            countdown(5);
+        }
+
+        public static void countdown(int num) {
+            for (int i = num; i > 0; i--) {
+                System.out.println(i);
+            }
+        }
+
+    }
---
+++ src/CountdownRecursive.java
@@ -0,0 +1,18 @@
+public class CountdownRecursive {
+
+    public static void main(String[] args) {
+
+        countdown(5);
+    }
+
+    public static void countdown(int num) {
+
+        if (num == 0) {
+            System.out.println(num);
+        } else{
+            System.out.println(num);
+            countdown(num - 1);
+        }
+    }
+
+}
